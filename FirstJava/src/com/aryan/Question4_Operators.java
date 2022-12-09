5907732b40d825ff9a1efd2b6c92ddd054778284
Aryan Agarwal <aryanagarwal607@gmail.com>
2022-12-10 01:45:56 +0530

    Question4_Operators added

---
+++ FirstJava/src/com/aryan/Question4_Operators.java
@@ -0,0 +1,27 @@
+package com.aryan;
+
+import java.util.Scanner;
+
+public class Question4_Operators {
+    public static void main(String[] args) {
+        // Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int a = input.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b = input.nextInt();
+        System.out.print("Enter Operator From '+','-','*','/': ");
+        String c = input.next();
+        if (c.equals("+")) {
+            System.out.println(a + b);
+        } else if (c.equals("-")) {
+            System.out.println(a - b);
+        } else if (c.equals("*")) {
+            System.out.println(a * b);
+        } else if (c.equals("/")) {
+            System.out.println(a / b);
+        } else {
+            System.out.println("Invalid choice");
+        }
+    }
+}
