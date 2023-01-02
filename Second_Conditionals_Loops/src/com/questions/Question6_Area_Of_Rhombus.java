4f6b98b84e0e666637a0d2866aa7e59dea6472d3
Aryan Agarwal <aryanagarwal607@gmail.com>
2023-01-02 17:39:12 +0530

    area of rhombus

---
+++ Second_Conditionals_Loops/src/com/questions/Question6_Area_Of_Rhombus.java
@@ -0,0 +1,15 @@
+package com.questions;
+
+import java.util.Scanner;
+
+public class Question6_Area_Of_Rhombus {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the diagonal 1 of Rhombus: ");
+        float p = input.nextInt();
+        System.out.print("Enter the diagonal 2 of Rhombus: ");
+        float q = input.nextInt();
+        float area = (p * q) / 2;
+        System.out.println("Area of parallelogram is " + area);
+    }
+}
