08e8b336b173744bdda62f217376bd5fed9176ca
Aryan Agarwal <aryanagarwal607@gmail.com>
2023-01-03 20:42:05 +0530

    New Answer added

---
+++ Second_Conditionals_Loops/src/com/questions/Question7_Area_Of_Equilateral_Triangle.java
@@ -0,0 +1,13 @@
+package com.questions;
+
+import java.util.Scanner;
+
+public class Question7_Area_Of_Equilateral_Triangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter side of equilateral triangle: ");
+        int a = input.nextInt();
+        double area = (0.433 * a * a);
+        System.out.println("Area of Equilateral Triangle: " + area);
+    }
+}
