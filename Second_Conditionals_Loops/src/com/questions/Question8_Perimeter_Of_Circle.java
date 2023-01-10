c209d224ce0017667b40bbcd0f2d0a54b0bf7cea
Aryan Agarwal <aryanagarwal607@gmail.com>
2023-01-10 13:52:38 +0530

    Question8 updated

--- Second_Conditionals_Loops/src/com/questions/Question8_Perimeter_Of_Circle.java
+++ Second_Conditionals_Loops/src/com/questions/Question8_Perimeter_Of_Circle.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Question8_Perimeter_Of_Circle {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter Radius of the Circle: ");
-        float radius= input.nextFloat();
-        float PI= (float) 3.14159;
-        float perimeter=2*PI*radius;
-        System.out.format("Perimeter of Circle = %.2f",perimeter);
+        float radius = input.nextFloat();
+        float PI = (float) 3.14159;
+        float perimeter = 2 * PI * radius;
+        System.out.format("Perimeter of Circle = %.2f", perimeter);
     }
 }
