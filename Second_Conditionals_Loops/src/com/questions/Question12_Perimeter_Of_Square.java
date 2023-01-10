e4a9a67d0ef519f86a2df97c909699c50e1ebfe8
Aryan Agarwal <aryanagarwal607@gmail.com>
2023-01-10 14:38:08 +0530

    Done

--- Second_Conditionals_Loops/src/com/questions/Question12_Perimeter_Of_Square.java
+++ Second_Conditionals_Loops/src/com/questions/Question12_Perimeter_Of_Square.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Question12_Perimeter_Of_Square {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-
+        System.out.print("Enter the side length of Square: ");
+        int side=input.nextInt();
+        System.out.println("Perimeter of Square = "+(4*side)+"cm");
     }
 }
